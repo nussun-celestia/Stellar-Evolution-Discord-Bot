1bf719e300bcf9b1ad1eeef90b17a30ceed4de00
nussun-celestia <69641920+nussun-celestia@users.noreply.github.com>
2022-08-28 14:14:40 +0200

    Add argument descriptions for plot

--- main.py
+++ main.py
@@ -55,6 +55,27 @@ def init():
             await ctx.send(f'```{stdout}```', file=discord.File('sse/evolve.dat'))
 
     @bot.hybrid_command()
+    @app_commands.describe(
+        # Descriptions borrowed from sse.f except for xbounds and ybounds
+        mass='mass is in solar units.',
+        z='z is metallicity in the range 0.0001 -> 0.03 where 0.02 is Population I.',
+        tphysf='tphysf is the maximum evolution time in Myr.',
+
+        xbounds='x-axis bounds for the plot.',
+        ybounds='y-axis bounds for the plot.',
+
+        neta='neta is the Reimers mass-loss coefficent (neta*4x10^-13; 0.5 normally).',
+        bwind='bwind is the binary enhanced mass loss parameter (inactive for single).',
+        hewind='hewind is a helium star mass loss factor (1.0 normally).',
+        sigma='sigma is the dispersion in the Maxwellian for the SN kick speed (190 km/s).',
+
+        ifflag='ifflag > 0 uses WD IFMR of HPE, 1995, MNRAS, 272, 800 (0).',
+        wdflag='wdflag > 0 uses modified-Mestel cooling for WDs (0).',
+        bhflag='bhflag > 0 allows velocity kick at BH formation (0).',
+        nsflag='nsflag > 0 takes NS/BH mass from Belczynski et al. 2002, ApJ, 572, 407 (1).',
+        mxns='mxns is the maximum NS mass (1.8, nsflag=0; 3.0, nsflag=1).',
+        idum='idum is the random number seed used in the kick routine.'
+    )
     async def plot(ctx, mass, z, tphysf,
                    xbounds='default', ybounds='default',
                    neta=0.5, bwind=0.0, hewind=0.5, sigma=190.0,
