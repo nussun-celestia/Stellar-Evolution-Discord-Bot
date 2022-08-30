a5c182f06a97184355e3be4cb82528a5dbc86e91
PotatoTIAB <serkanicoz2005@gmail.com>
2022-08-30 14:53:20 +0300

    amend! removed debug statements, this is what happens when i skip my morning coffee.
    added colour.

--- generate_embed.py
+++ generate_embed.py
@@ -2,14 +2,6 @@ import re
 from discord import Embed
 
 def generate_embed(stdout: str):
-    stdout = """Main sequence Star            Time        0.0 Mass   5.000
-Hertzsprung Gap               Time       87.8 Mass   5.000
-Core Helium Burning           Time       88.3 Mass   4.999
-First AGB                     Time       98.8 Mass   4.961
-Second AGB                    Time       99.2 Mass   4.935
-Carbon/Oxygen WD              Time      100.1 Mass   1.200
-Carbon/Oxygen WD              Time    12000.0 Mass   1.200"""
-
     matches = re.findall("(.+\\b)\s+Time\s+([\d.]+)\s+Mass\s+([\d.]+)", stdout)
 
     values = ["", "", ""]
@@ -28,6 +20,3 @@ Carbon/Oxygen WD              Time    12000.0 Mass   1.200"""
     return embed
 
 
-
-if __name__ == "__main__":
-    generate_embed("")
