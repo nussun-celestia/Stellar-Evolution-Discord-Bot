2b57806916651c5333cbf37fde7d1798ac6376b9
PotatoTIAB <serkanicoz2005@gmail.com>
2022-08-30 13:46:53 +0300

    now matches from the stdout.

--- generate_embed.py
+++ generate_embed.py
@@ -1,3 +1,5 @@
+import re
+
 def generate_embed(stdout: str):
     stdout = """Main sequence Star            Time        0.0 Mass   5.000
 Hertzsprung Gap               Time       87.8 Mass   5.000
@@ -6,7 +8,10 @@ First AGB                     Time       98.8 Mass   4.961
 Second AGB                    Time       99.2 Mass   4.935
 Carbon/Oxygen WD              Time      100.1 Mass   1.200
 Carbon/Oxygen WD              Time    12000.0 Mass   1.200"""
-    print(stdout)
+
+    matches = re.findall("(.+\\b)\s+Time\s+([\d.]+)\s+Mass\s+([\d.]+)", stdout)
+    print(matches)    
+
 
 
 if __name__ == "__main__":
