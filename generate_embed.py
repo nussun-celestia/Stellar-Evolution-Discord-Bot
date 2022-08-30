ee2592ccf0e274a706dfd5b428b639365aac5d53
nussun-celestia <69641920+nussun-celestia@users.noreply.github.com>
2022-08-30 21:37:20 +0200

    Simplify using numpy

--- generate_embed.py
+++ generate_embed.py
@@ -1,22 +1,16 @@
 from discord import Embed
+import numpy as np
 import re
 
 
 def generate_embed(stdout: str, title: str):
     matches = re.findall('(.+\\b)\s+Time\s+([\d.]+)\s+Mass\s+([\d.]+)', stdout)
 
-    values = ['', '', '']
-    for line in matches:
-        for i in range(3):
-            values[i] += line[i].strip() + '\n'
-    
-    for i in range(3):
-        values[i] = values[i][:-1]
+    columns = ['\n'.join(np.char.strip(column)) for column in zip(*matches)]
 
     embed = Embed(title=title, colour=0xCCDFF0)
-    embed.add_field(name='Stage', value=values[0], inline=True)
-    embed.add_field(name='Time', value=values[1], inline=True)
-    embed.add_field(name='Mass', value=values[2], inline=True)
+    embed.add_field(name='Stage', value=columns[0], inline=True)
+    embed.add_field(name='Time', value=columns[1], inline=True)
+    embed.add_field(name='Mass', value=columns[2], inline=True)
 
     return embed
-    
