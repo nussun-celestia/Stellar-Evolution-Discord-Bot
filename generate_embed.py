a86b70e5bcc463585395907ffd8d30c58469ef26
PotatoTIAB <serkanicoz2005@gmail.com>
2022-08-30 13:18:44 +0300

    added new function file.

---
+++ generate_embed.py
@@ -0,0 +1,13 @@
+def generate_embed(stdout: str):
+    stdout = """Main sequence Star            Time        0.0 Mass   5.000
+Hertzsprung Gap               Time       87.8 Mass   5.000
+Core Helium Burning           Time       88.3 Mass   4.999
+First AGB                     Time       98.8 Mass   4.961
+Second AGB                    Time       99.2 Mass   4.935
+Carbon/Oxygen WD              Time      100.1 Mass   1.200
+Carbon/Oxygen WD              Time    12000.0 Mass   1.200"""
+    print(stdout)
+
+
+if __name__ == "__main__":
+    generate_embed("")
