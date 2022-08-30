5e8c01daabd7f457cdb2851a0bf8dd2f89bf4c69
nussun-celestia <69641920+nussun-celestia@users.noreply.github.com>
2022-08-30 20:21:11 +0200

    Small changes

--- generate_embed.py
+++ generate_embed.py
@@ -8,16 +8,15 @@ def generate_embed(stdout: str, title: str):
     values = ['', '', '']
     for line in matches:
         for i in range(3):
-            values[i] += line[i] + '\n'
+            values[i] += line[i].strip() + '\n'
     
     for i in range(3):
         values[i] = values[i][:-1]
 
-    embed = Embed(title=title, colour=0x00FF00)
+    embed = Embed(title=title, colour=0xCCDFF0)
     embed.add_field(name='Stage', value=values[0], inline=True)
     embed.add_field(name='Time', value=values[1], inline=True)
     embed.add_field(name='Mass', value=values[2], inline=True)
 
     return embed
-
-
+    
