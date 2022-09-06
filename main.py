f7ec5050ea1ac5e219722f0b013a7c9dc2f607f8
nussun-celestia <69641920+nussun-celestia@users.noreply.github.com>
2022-09-06 06:49:54 +0200

    Refactor

--- main.py
+++ main.py
@@ -26,25 +26,42 @@ def init():
         await bot.tree.sync()
         await ctx.send('Successfully synced commands.')
 
+    async def handle_evolv1_error(interaction: discord.Interaction, stdout: str) -> None:
+        try:
+            raise EVOLV1Error('An error has occured in EVOLV1; try using different parameters')
+        except EVOLV1Error as e:
+            embed = generate_embed(stdout, e, title='Error')
+            await interaction.response.send_message(embed=embed)
+
+    async def handle_nonpositive_mass(interaction: discord.Interaction) -> None:
+        try:
+            raise NotImplementedError('Negative or zero mass currently not supported')
+        except NotImplementedError as e:
+            embed = generate_embed('', e, title='Error')
+            await interaction.response.send_message(embed=embed)
+
+    def sse_input_descriptions(func):
+        return app_commands.describe(
+            # Descriptions borrowed from sse.f
+            mass='mass is in solar units.',
+            z='z is metallicity in the range 0.0001 -> 0.03 where 0.02 is Population I.',
+            tphysf='tphysf is the maximum evolution time in Myr.',
+
+            neta='neta is the Reimers mass-loss coefficent (neta*4x10^-13; 0.5 normally).',
+            bwind='bwind is the binary enhanced mass loss parameter (inactive for single).',
+            hewind='hewind is a helium star mass loss factor (1.0 normally).',
+            sigma='sigma is the dispersion in the Maxwellian for the SN kick speed (190 km/s).',
+
+            ifflag='ifflag > 0 uses WD IFMR of HPE, 1995, MNRAS, 272, 800 (0).',
+            wdflag='wdflag > 0 uses modified-Mestel cooling for WDs (0).',
+            bhflag='bhflag > 0 allows velocity kick at BH formation (0).',
+            nsflag='nsflag > 0 takes NS/BH mass from Belczynski et al. 2002, ApJ, 572, 407 (1).',
+            mxns='mxns is the maximum NS mass (1.8, nsflag=0; 3.0, nsflag=1).',
+            idum='idum is the random number seed used in the kick routine.'
+        )(func)
+
     @bot.tree.command()
-    @app_commands.describe(
-        # Descriptions borrowed from sse.f
-        mass='mass is in solar units.',
-        z='z is metallicity in the range 0.0001 -> 0.03 where 0.02 is Population I.',
-        tphysf='tphysf is the maximum evolution time in Myr.',
-
-        neta='neta is the Reimers mass-loss coefficent (neta*4x10^-13; 0.5 normally).',
-        bwind='bwind is the binary enhanced mass loss parameter (inactive for single).',
-        hewind='hewind is a helium star mass loss factor (1.0 normally).',
-        sigma='sigma is the dispersion in the Maxwellian for the SN kick speed (190 km/s).',
-
-        ifflag='ifflag > 0 uses WD IFMR of HPE, 1995, MNRAS, 272, 800 (0).',
-        wdflag='wdflag > 0 uses modified-Mestel cooling for WDs (0).',
-        bhflag='bhflag > 0 allows velocity kick at BH formation (0).',
-        nsflag='nsflag > 0 takes NS/BH mass from Belczynski et al. 2002, ApJ, 572, 407 (1).',
-        mxns='mxns is the maximum NS mass (1.8, nsflag=0; 3.0, nsflag=1).',
-        idum='idum is the random number seed used in the kick routine.'
-    )
+    @sse_input_descriptions
     async def evolve(interaction: discord.Interaction,
                      mass: float, z: float, tphysf: float,
                      neta: float=0.5, bwind: float=0.0, hewind: float=0.5, sigma: float=190.0,
@@ -57,43 +74,19 @@ def init():
                                           pts1=pts1, pts2=pts2, pts3=pts3)
             stdout = await sse.run_sse()
             if 'ERROR' in stdout:
-                try:
-                    raise EVOLV1Error('An error has occured in EVOLV1; try using different parameters')
-                except EVOLV1Error as e:
-                    embed = generate_embed(stdout, e, title='Error')
-                    await interaction.response.send_message(embed=embed)
+                await handle_evolv1_error(interaction, stdout)
             else:
                 embed = generate_embed(stdout, title='Output')
                 await interaction.response.send_message(embed=embed)
                 await interaction.channel.send(file=discord.File(f'{sse.SSE_FOLDER}/evolve.dat'))
         else:
-            try:
-                raise NotImplementedError('Negative or zero mass currently not supported')
-            except NotImplementedError as e:
-                embed = generate_embed(e, title='Error')
-                await interaction.response.send_message(embed=embed)
+            await handle_nonpositive_mass(interaction)
 
     @bot.tree.command()
+    @sse_input_descriptions
     @app_commands.describe(
-        # Descriptions borrowed from sse.f except for xbounds and ybounds
-        mass='mass is in solar units.',
-        z='z is metallicity in the range 0.0001 -> 0.03 where 0.02 is Population I.',
-        tphysf='tphysf is the maximum evolution time in Myr.',
-
         xbounds='x-axis bounds for the plot.',
         ybounds='y-axis bounds for the plot.',
-
-        neta='neta is the Reimers mass-loss coefficent (neta*4x10^-13; 0.5 normally).',
-        bwind='bwind is the binary enhanced mass loss parameter (inactive for single).',
-        hewind='hewind is a helium star mass loss factor (1.0 normally).',
-        sigma='sigma is the dispersion in the Maxwellian for the SN kick speed (190 km/s).',
-
-        ifflag='ifflag > 0 uses WD IFMR of HPE, 1995, MNRAS, 272, 800 (0).',
-        wdflag='wdflag > 0 uses modified-Mestel cooling for WDs (0).',
-        bhflag='bhflag > 0 allows velocity kick at BH formation (0).',
-        nsflag='nsflag > 0 takes NS/BH mass from Belczynski et al. 2002, ApJ, 572, 407 (1).',
-        mxns='mxns is the maximum NS mass (1.8, nsflag=0; 3.0, nsflag=1).',
-        idum='idum is the random number seed used in the kick routine.'
     )
     async def plot(interaction: discord.Interaction,
                    mass: float, z: float, tphysf: float,
@@ -108,11 +101,7 @@ def init():
                                           pts1=pts1, pts2=pts2, pts3=pts3)
             stdout = await sse.run_sse()
             if 'ERROR' in stdout:
-                try:
-                    raise EVOLV1Error('An error has occured in EVOLV1; try using different parameters')
-                except EVOLV1Error as e:
-                    embed = generate_embed(stdout, e, title='Error')
-                    await interaction.response.send_message(embed=embed)
+                await handle_evolv1_error(interaction, stdout)
             else:
                 await sse_plot.sse_plot(xbounds, ybounds)
                 embed = generate_embed(stdout, title='Output')
@@ -120,11 +109,7 @@ def init():
                 embed.set_image(url=f'attachment://hrdiag.png')
                 await interaction.response.send_message(file=file, embed=embed)
         else:
-            try:
-                raise NotImplementedError('Negative or zero mass currently not supported')
-            except NotImplementedError as e:
-                embed = generate_embed(e, title='Error')
-                await interaction.response.send_message(embed=embed)
+            await handle_nonpositive_mass(interaction)
 
     bot.run(TOKEN)
 
